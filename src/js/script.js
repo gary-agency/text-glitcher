9d4b96fa7c7e26d45c8a1a50641ac1d832910171
GARY <justine@gary.agency>
2019-05-21 22:47:59 +0200

    base project

---
+++ src/js/script.js
@@ -0,0 +1,3 @@
+$(function() {
+    $('.glitch-me').textGlitcher({});
+});
---
+++ src/js/textGlitcher.jquery.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $.fn.textGlitcher = function (options) {
+
+        var settings = $.extend({
+            autoplay: false,
+        }, options);
+        console.log(settings);
+
+        return this.each(function () {
+
+        });
+    };
+})(jQuery);
